dc157c9fbb6998e5efdd40ae4dddbc3c3f2ab7e8
Maptaul <maptaulislam1@gmail.com>
2026-08-19 23:06:17 +0600

    feat: implement profile image upload functionality in user module

--- src/app/module/user/user.route.ts
+++ src/app/module/user/user.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
+import { UserController } from "./user.controller";
 
 const router = Router();
 
-router.patch("/profile-image");
+router.patch("/profile-image", UserController.uploadProfileImage);
 export const UserRoutes = router;
