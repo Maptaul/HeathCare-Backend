1ee7fb7c786c4e26319e86c08943a91248c1a6f8
Maptaul <maptaulislam1@gmail.com>
2026-08-22 15:39:09 +0600

    feat: add profile image handling to user module with database migration

--- src/app/module/user/user.controller.ts
+++ src/app/module/user/user.controller.ts
@@ -2,13 +2,21 @@ import { Request, Response } from "express";
 import httpStatus from "http-status";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
+import { UserService } from "./user.service";
 
 const uploadProfileImage = catchAsync(async (req: Request, res: Response) => {
+  if (!req.file) {
+    throw new Error("No file uploaded");
+  }
+
+  const userId = req.user?.userId;
+  const result = await UserService.uploadProfileImage(req.file.buffer, userId!);
+
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: "Profile image uploaded successfully",
-    data: null,
+    data: result,
   });
 });
 
